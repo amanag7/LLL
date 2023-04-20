34f967e76cd8e7b9126f68b22edd9edfef7bd248
amanag7 <cs21mtech11006@iith.ac.in>
2023-04-20 11:39:50 +0530

    adding prompts code

--- lamol_adapter_prompt/test.sh
+++ lamol_adapter_prompt/test.sh
@@ -6,3 +6,7 @@ python test.py \
   --data_dir $DATA_DIR \
   --model_dir_root $MODEL_ROOT_DIR \
   "$@"
+
+nohup python test_adapter_from_scratch.py --data_dir data --model_dir models --seq_train_type lll --model_name gpt2 --n_gpus 1 --n_workers 25 --n_train_epochs 12 --gen_lm_sample_percentage 0.0 --tasks sst srl woz.en --lm_lambda 0.0 > Testlogs_3og_adap.out
+
+nohup python test_adapter_from_scratch.py --data_dir data --model_dir new_models --seq_train_type lll --model_name gpt2 --n_gpus 1 --n_workers 25 --n_train_epochs 12 --gen_lm_sample_percentage 0.0 --tasks sst srl woz.en --lm_lambda 0.0 > Testlogs_3og_preadap.out
--- lamol_adapter_prompt/train.sh
+++ lamol_adapter_prompt/train.sh
@@ -6,3 +6,13 @@ python train.py \
   --data_dir $DATA_DIR \
   --model_dir_root $MODEL_ROOT_DIR \
   "$@"
+
+nohup python train_experiment_adapter_from_scratch_mam_config.py --data_dir data --model_dir_root models --seq_train_type lll --model_name gpt2 --n_gpus 8 --n_workers 75 --fp32 --n_train_epochs 4 --gen_lm_sample_percentage 0.00 --tasks sst srl woz.en --lm_lambda 0.0 > Trainlogs_3decaNLP_mam_config.out
+
+nohup python train_experiment_adapter_prompts.py --data_dir data --model_dir_root models --seq_train_type lll --model_name gpt2 --n_gpus 8 --n_workers 75 --fp32 --n_train_epochs 2 --gen_lm_sample_percentage 0.00 --tasks sst srl woz.en --lm_lambda 0.0 > Trainlogs_3og_prompts.out
+
+nohup python train_experiment_prefix_adapter.py --data_dir data --model_dir_root new_models --seq_train_type lll --model_name gpt2 --n_gpus 8 --n_workers 75 --fp32 --n_train_epochs 2 --gen_lm_sample_percentage 0.00 --tasks sst srl woz.en --lm_lambda 0.0 > Trainlogs_3og_preadap.out
+
+nohup python train_experiment_adapter_from_scratch_mam_config.py --data_dir data --model_dir_root models --seq_train_type lll --model_name gpt2 --n_gpus 2 --n_workers 75 --fp32 --n_train_epochs 12 --gen_lm_sample_percentage 0.00 --tasks sst srl woz.en --lm_lambda 0.0 > Trainlogs_3og_adap.out
+
+nohup python train_experiment_prefix_adapter.py --data_dir data --model_dir_root new_models --seq_train_type lll --model_name gpt2 --n_gpus 2 --n_workers 75 --fp32 --n_train_epochs 12 --gen_lm_sample_percentage 0.00 --tasks sst srl woz.en --lm_lambda 0.0 > Trainlogs_3og_preadap.out
